1bbbb8040d18f9183f933ff673c59cbf58cb6b8c
Kn0wn-Un <naikdarsh175@gmail.com>
2020-09-04 12:59:00 +0530

    grayscale patch

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let btn = document.querySelector("#clear");
-btn.addEventListener("click", ()=>{location.reload()});
-
+btn.addEventListener("click", () => {location.reload()});
 
 function makeFrame(numGrids = 16){
     let area = document.querySelector("#sketchArea");
     area.setAttribute("value", "white");
+    area.innerHTML="";
     let rows="";
     let col="";
     for(let i = 0; i < numGrids; i++)
@@ -22,46 +22,9 @@ function makeFrame(numGrids = 16){
         area.appendChild(box);
     }
     let btn = document.querySelector("#toggle");
-    toggleColor()
+    toggleColor();
 }
 makeFrame();
-
-
-
-
-btn = document.querySelector("#toggle");
-btn.addEventListener("click", toggleColor);
-function toggleColor(){
-    let box = document.querySelectorAll(".grid-item");
-    let area = document.querySelector("#sketchArea");
-    let btnn = document.querySelector("#toggle");
-    box.forEach(one => {
-            one.addEventListener("mouseover", colorChanger);
-    });
-    if(area.getAttribute("value") === "black"){
-        area.setAttribute("value", "white");
-        btnn.style.backgroundColor="white";
-        btnn.style.color="black";
-    }
-    else{
-        area.setAttribute("value", "black");
-        btnn.style.backgroundColor="black";
-        btnn.style.color="white";
-    }
-}
-function colorChanger(){
-    console.log(this.style.backgroundColor)
-    let area = document.querySelector("#sketchArea");
-    if(area.getAttribute("value") === "black"){
-        this.style.backgroundColor = "rgba(0, 0, 0, 1)";
-        this.setAttribute("value", "1");
-    }
-    else{
-        this.style.backgroundColor = "rgba(255, 255, 255, 0)"; 
-        this.setAttribute("value", "0");
-    }   
-}
-
 
 
 
@@ -81,14 +44,59 @@ function getInput(){
 }
 
 
+
+btn = document.querySelector("#toggle");
+btn.addEventListener("click", toggleColor);
+function toggleColor(){
+    let box = document.querySelectorAll(".grid-item");
+    let area = document.querySelector("#sketchArea");
+    let btnn = document.querySelector("#toggle");
+    let greyBtn = document.querySelector("#gray");
+    box.forEach(one => {
+        one.removeEventListener("mouseover", setRgba);
+        one.addEventListener("mouseover", colorChanger);
+    });
+    if(area.getAttribute("value") === "black"){
+        area.setAttribute("value", "white");
+        btnn.style.backgroundColor="white";
+        btnn.style.color="black";
+        greyBtn.style.border="1px solid grey";
+    }
+    else{
+        area.setAttribute("value", "black");
+        btnn.style.backgroundColor="black";
+        btnn.style.color="white";
+        greyBtn.style.border="1px solid grey";
+    }
+}
+function colorChanger(){
+    console.log(this.style.backgroundColor)
+    let area = document.querySelector("#sketchArea");
+    if(area.getAttribute("value") === "black"){
+        this.style.backgroundColor = "rgba(0, 0, 0, 1)";
+        this.setAttribute("value", "1");
+    }
+    else{
+        this.style.backgroundColor = "rgba(255, 255, 255, 0)"; 
+        this.setAttribute("value", "0");
+    }   
+}
+
+
+
 btn = document.querySelector("#gray");
 btn.addEventListener("click", grey);
 function grey(){
     let box = document.querySelectorAll(".grid-item");
+    let greyBtn = document.querySelector("#gray");
+    let area = document.querySelector("#sketchArea");
+    area.setAttribute("value", "black");
+    toggleColor();
     box.forEach(one => {
         one.removeEventListener("mouseover", colorChanger);
         one.addEventListener("mouseover", setRgba);
     });
+    greyBtn.style.border="2px solid green";
 }
 function setRgba(){
     aValue = parseFloat(this.getAttribute("value"));
@@ -103,4 +111,3 @@ function setRgba(){
     this.style.backgroundColor = "rgba(0, 0, 0, "+ aValue + ")"; 
     this.setAttribute("value", aValue);
 }
- 
